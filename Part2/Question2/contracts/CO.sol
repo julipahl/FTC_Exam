d118e936f6fe4f8a28f750c41e2d9fe1c61144fe
Julika Pahl <julipahl@gmail.com>
2019-08-03 21:41:15 +0200

    added question2

---
+++ Part2/Question2/contracts/CO.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+// import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract CO is Ownable {
+    
+
+}
