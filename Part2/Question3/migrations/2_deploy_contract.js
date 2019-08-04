4169282abdfdc783fde134173a3b2b2be8cb548a
Julika Pahl <julipahl@gmail.com>
2019-08-04 23:56:32 +0200

    updates

--- Part2/Question3/migrations/2_deploy_contract.js
+++ Part2/Question3/migrations/2_deploy_contract.js
@@ -1,12 +1,21 @@
-const COToken = artifacts.require("./COToken.sol");
-const CoShoe = artifacts.require("./CoShoe.sol");
+// const COToken = artifacts.require("./COToken.sol");
+// const CoShoe = artifacts.require("./CoShoe.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(COToken, "COToken", "CO", 0).then((contractInstance) => {
-    deployer.deploy(CoShoe, contractInstance.address);
-  })
+// module.exports = function(deployer) {
+//     deployer.deploy(COToken, "COToken", "CO", 0).then((contractInstance) => {
+//     deployer.deploy(CoShoe, contractInstance.address);
+//   })
 
 // }
+
+var COToken = artifacts.require("./COToken.sol");
+var CoShoe = artifacts.require("./CoShoe.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(COToken, "CoToken", "CO", 0).then(function(){
+        return deployer.deploy(CoShoe, COToken.address)
+});
+};
 
 // module.exports = async function(deployer) {
 //   const COTokenInstance = await deployer.deploy(COToken, "CoToken", "CO", 0);
